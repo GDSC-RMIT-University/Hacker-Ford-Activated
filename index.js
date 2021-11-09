c6de581a87600c4668cd3593fb1f198e67feb869
Yi Jie Chuah <s3847905@student.rmit.edu.au>
2021-11-10 00:26:23 +1100

    Basic looped daily reminder setup.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const Discord = require('discord.js');
 const {Client, Intents} = require('discord.js');
 const {guildId, token} = require('./config.json');
-const Discord = require('discord.js');
 
 // Create a new client instance
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, 'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_MESSAGES',
-        'DIRECT_MESSAGES', Intents.FLAGS.GUILD_MESSAGES]
+    intents: [Intents.FLAGS.GUILDS, 
+        'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_MESSAGES',
+        'DIRECT_MESSAGES', 
+        Intents.FLAGS.GUILD_MESSAGES]
 });
 
 var role = ''
@@ -16,7 +18,7 @@ client.once('ready', () => {
 });
 
 // The bot going a bit nuts whenever someone says a specific word
-client.on('message', message => {
+client.on('messageCreate', message => {
 
     if (message.author.bot) return;
 
@@ -24,7 +26,7 @@ client.on('message', message => {
     role = guild.roles.cache.find(r => r.name === "@everyone");
 
     if(message.content.includes('hi')){
-        message.channel.send('\'sup.');
+        message.channel.send('\'sup. ');
     } else if (message.content.includes('hello')){
         message.channel.send('How you doin\'?');
     } else if ( message.mentions.has(client.user)){
@@ -32,6 +34,26 @@ client.on('message', message => {
     }
 });
 
+//Reminder Feature
+const ROLE_DEV_CHANNEL_ID = "907410177463042118"
+const CHECK_INTERVAL = 500
+const sendReminder = (channel,message) => {
+    if (channel.isText()) {
+        channel.send(message)
+            .then(console.log("Reminder Sent!"))
+            .catch(console.error);
+    }
+}
+client.once('ready', () => {
+    setInterval(() => {
+        var currDate = new Date();
+        if (currDate.getHours() == "0"){
+            client.channels.fetch(ROLE_DEV_CHANNEL_ID)
+                .then(channel => sendReminder(channel,"Testing Reminder"))
+                .catch(console.error);
+        }
+    }, CHECK_INTERVAL)
+})
 
 // Login to Discord with your client's token
 client.login(token);
