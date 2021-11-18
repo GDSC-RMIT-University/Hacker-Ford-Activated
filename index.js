ae30578554152d51bd723c5a8cdb1959b4ec6672
jacqueline-ann-lim-s3753072 <jacquelineanncolim@gmail.com>
2021-11-18 14:01:20 +1100

    Created /book slash command

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 // Create a new client instance
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, 'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_MESSAGES',
-        'DIRECT_MESSAGES', Intents.FLAGS.GUILD_MESSAGES]
+        'DIRECT_MESSAGES', Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
 });
 
 var role = ''
@@ -13,6 +13,40 @@ var role = ''
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
+
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'book',
+        description: 'Book a mentoring session!',
+        options: [
+            {
+                name: 'team_number',
+                description: 'Enter your team number',
+                required: true,
+                type: 'INTEGER',
+            },
+            {
+                name: 'describe_the_issue',
+                description: 'Describe the issue that you need help on',
+                required: true,
+                type: 'STRING',
+            },
+            {
+                name: 'preferred_mentor',
+                description: 'Enter your preferred mentor if you have',
+                type: 'MENTIONABLE',
+                required: false,
+            },
+        ],
+    })
 });
 
 // The bot going a bit nuts whenever someone says a specific word
@@ -32,6 +66,15 @@ client.on('message', message => {
     }
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const {commandName, options} = interaction;
+
+    if (commandName === 'book') {
+
+    } 
+});
 
 // Login to Discord with your client's token
 client.login(token);
