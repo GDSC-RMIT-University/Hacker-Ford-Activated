d38de5f454262741b734cae4be7c9d7b153c3145
jacqueline-ann-lim-s3753072 <jacquelineanncolim@gmail.com>
2021-11-18 14:17:24 +1100

    Created /book slash command

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS, 
         'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_MESSAGES',
         'DIRECT_MESSAGES', 
-        Intents.FLAGS.GUILD_MESSAGES]
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
 });
 
 var role = ''
@@ -16,6 +17,40 @@ var role = ''
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
+
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'book',
+        description: 'Book a mentoring session!',
+        options: [
+            {
+                name: 'team_number',
+                description: 'Enter your team number',
+                required: true,
+                type: 'INTEGER',
+            },
+            {
+                name: 'describe_the_issue',
+                description: 'Describe the issue that you need help on',
+                required: true,
+                type: 'STRING',
+            },
+            {
+                name: 'preferred_mentor',
+                description: 'Enter your preferred mentor if you have',
+                type: 'MENTIONABLE',
+                required: false,
+            },
+        ],
+    })
 });
 
 // The bot going a bit nuts whenever someone says a specific word
@@ -66,6 +101,16 @@ client.once('ready', () => {
         })
     }, CHECK_INTERVAL)
 })
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'book'){
+
+    }
+});
 
 // Login to Discord with your client's token
 client.login(token);
